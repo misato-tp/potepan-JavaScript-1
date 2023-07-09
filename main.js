54d73d179c945f71322c5e175c5238b4eeb261b0
EC2 Default User <ec2-user@ip-172-31-5-155.us-east-2.compute.internal>
2023-07-09 15:02:09 +0000

    ストップウォッチ課題

---
+++ main.js
@@ -0,0 +1,44 @@
+const timeCount = document.getElementById('timeCount');
+const startButton = document.getElementById('start');
+const stopButton = document.getElementById('stop');
+const resetButton = document.getElementById('reset');
+
+let startTime;
+let stopTime = 0;
+let timeoutID;
+
+function displayTime(){
+    const currentTime = new Date(Date.now() - startTime + stopTime);
+    const h = String(currentTime.getHours()-9).padStart(1,'0');
+    const m = String(currentTime.getMinutes()).padStart(1,'0');
+    const s = String(currentTime.getSeconds()).padStart(1,'0');
+    const ms = String(currentTime.getMilliseconds()).padStart(3,'0');
+    
+    
+    timeCount.textContent=`${h}:${m}:${s}.${ms}`;
+    timeoutID = setTimeout(displayTime,10);
+}
+
+startButton.addEventListener('click',() =>{
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    resetButton.disabled = true;
+    startTime = Date.now();
+    displayTime();
+});
+
+stopButton.addEventListener('click', function(){
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = false;
+    clearTimeout(timeoutID);
+    stopTime += (Date.now() - startTime);
+});
+
+resetButton.addEventListener('click', function(){
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    resetButton.disabled = true;
+    timeCount.textContent = '0:0:0:0';
+    stopTime = 0;
+});
